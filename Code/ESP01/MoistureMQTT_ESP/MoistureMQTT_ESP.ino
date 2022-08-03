1e908ce76bb3cd85d31c304b5a5ae72209d9039b
Cybernetic <cybernetic.msg@gmail.com>
2022-08-03 22:34:18 +0200

    Temperature integration (#2)
    * Modify code to run tests quickly
    
    * create sketch to test sensor
    
    * get initial code
    
    * integratin failed - Serial not working properly
    
    * Serial improvements
    
    * deleted unnecessary ino sketch
    
    * Improve Serial Com
    
    * fix issue for wake up

--- Code/ESP01/MoistureMQTT_ESP/MoistureMQTT_ESP.ino
+++ Code/ESP01/MoistureMQTT_ESP/MoistureMQTT_ESP.ino
@@ -8,79 +8,98 @@
 //
 // Cybernetic 06/2022
 // Running on ESP01
-// Description:
-//      1/ Connect to wifi
-//      2/ Tell to the Attiny, "I am Ready"
-//      3/ Wait to receive any data separate by \n
-//      4/ Publish them via MQTT on topic_UP topic
-//
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
 #include "credentials.h"
 
+//// VARIABLES ////
+// MQTT
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
 const char *mqtt_server = MQTT_SERVER_URL;
 const char *HostName = "Moisture_Sensor_01";
 const char *topic_UP = "DATA0";
-char *statusMsg = "";
-
 WiFiClient espClient;
 PubSubClient client(espClient);
+// Time
+unsigned long previous_millis = 0;
+// Serial com
+const byte numChars = 32;
+boolean newData = false;
+char receivedChars[numChars];
+char *statusMsg = "";
 
-void setup_wifi() {
-    delay(10);
-    /*Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);*/
-    WiFi.mode(WIFI_STA);
-    WiFi.setHostname(HostName);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        // Serial.print(".");
-        delay(500);
-    }
-    /*Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());*/
-}
-void reconnect() {
-    while (!client.connected()) {
-        // Serial.print("Attempting MQTT connection...");
-        String clientId = "ESP8266Client-";
-        clientId += String(random(0xffff), HEX);
-        if (client.connect(clientId.c_str())) {
-            // Serial.println("connected");
-        } else {
-            /*Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" try again in 5 seconds");*/
-            delay(5000);
-        }
-    }
-}
 void setup() {
     Serial.begin(9600);
     setup_wifi();
     client.setServer(mqtt_server, 1883);
-    statusMsg = "Ready";
+    statusMsg = "<Ready>";
 }
 
 void loop() {
     client.loop();
     if (client.connected()) {
-        Serial.println(statusMsg);
-        if (Serial.available() > 0) {
-            String msg = Serial.readStringUntil('\n');
-            char Buf[msg.length() + 1];
-            msg.toCharArray(Buf, msg.length() + 1);
-            client.publish(topic_UP, Buf);
-            statusMsg = "Sent";
+        unsigned long now = millis();
+        recvWithStartEndMarkers();
+        if (now - previous_millis >= 200) {
+            Serial.println(statusMsg);
+            previous_millis = now;
+        }
+        if (newData) {
+            newData = false;
+            client.publish(topic_UP, receivedChars);
+            statusMsg = "<Sent>";
         }
     } else {
         reconnect();
+    }
+}
+
+void setup_wifi() {
+    delay(10);
+    WiFi.mode(WIFI_STA);
+    WiFi.setHostname(HostName);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+}
+
+void reconnect() {
+    while (!client.connected()) {
+        String clientId = "ESP8266Client-";
+        clientId += String(random(0xffff), HEX);
+        if (!client.connect(clientId.c_str())) {
+            delay(5000);
+        }
+    }
+}
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            } else {
+                receivedChars[ndx] = '\0';  // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        } else if (rc == startMarker) {
+            recvInProgress = true;
+        }
     }
 }
